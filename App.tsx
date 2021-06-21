e7338498c5770fd1cb4acd031c43ab003131c784
Cristiano Lima <52179531+K16bits@users.noreply.github.com>
2021-06-21 19:41:16 -0300

    init

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {SignIn} from './src/screens/SignIn'; // se usar export default, precisara remover as chaves
+
+export default function App() {
+  return (
+      <SignIn/>
+  );
+}
+
---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export function SignIn() {
+    return (
+        <View>
+            <Text>Hello world, NLW Together</Text>
+        </View>
+    );
+}
