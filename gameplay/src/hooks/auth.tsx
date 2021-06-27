6214358fc8f1526896c0237a7cb6778a3cababef
Cristiano Lima <52179531+K16bits@users.noreply.github.com>
2021-06-27 12:58:04 -0300

    Refactoring - 5 dia

--- gameplay/src/hooks/auth.tsx
+++ gameplay/src/hooks/auth.tsx
@@ -6,13 +6,12 @@ import React, {
 } from 'react';
 
 import * as AuthSession from 'expo-auth-session';
-import {
-    SCOPE,
-    CLIENT_ID,
-    CDN_IMAGE,
-    REDIRECT_URI,
-    RESPONSE_TYPE
-} from '../configs'
+const { SCOPE } = process.env;
+const { CLIENT_ID } = process.env;
+const { CDN_IMAGE } = process.env;
+const { REDIRECT_URI } = process.env;
+const { RESPONSE_TYPE } = process.env;
+
 import { api } from '../services/api';
 
 type User = {
@@ -37,7 +36,8 @@ type AuthProviderProps = {
 
 type AuthorizationResponse = AuthSession.AuthSessionResult & {
     params: {
-        access_token: string;
+        access_token?: string;
+        error?: string;
     }
 }
 
@@ -54,24 +54,23 @@ function AuthProvide({ children }: AuthProviderProps) {
             const { type, params } = await AuthSession
                 .startAsync({ authUrl }) as AuthorizationResponse;
 
-            if (type === "success") {
+            if (type === "success" && !params.error) {
                 api.defaults.headers.authorization = `Bearer ${params.access_token}`;
 
                 const userInfo = await api.get('/users/@me');
                 const firstName = userInfo.data.username.split(' ')[0];
                 userInfo.data.avatar = `${CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`
-                
+
                 setUser({
                     ...userInfo.data,
                     firstName,
                     token: params.access_token
                 });
-                setLoading(false);
-            } else {
-                setLoading(false)
             }
         } catch {
             throw new Error('Não foi possivel autenticar')
+        } finally {
+            setLoading(false)
         }
     }
 
